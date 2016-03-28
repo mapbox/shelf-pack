7b9d598748431711921ff82bbd2fd2ebb92835e9
Bryan Housel <bryan@mapbox.com>
2016-03-28 16:17:50 -0400

    Document everything, use options hashes for parameter passing The options hashes are easier to work than explict argument lists, but have a measurable performance hit and will be reverted.

--- bench/bench.js
+++ bench/bench.js
@@ -22,10 +22,10 @@ var fixedBoth = [],
 for (var i = 0; i < N; i++) {
     w = randSize();
     h = randSize();
-    fixedBoth.push({width: 12, height: 12});
-    randWidth.push({width: w, height: 12});
-    randHeight.push({width: 12, height: h});
-    randBoth.push({width: w, height: h});
+    fixedBoth.push({ width: 12, height: 12 });
+    randWidth.push({ width: w, height: 12 });
+    randHeight.push({ width: 12, height: h });
+    randBoth.push({ width: w, height: h });
 }
 
 
@@ -33,36 +33,36 @@ var suite = new Benchmark.Suite();
 
 suite
     .add('ShelfPack allocate fixed bins', function(t) {
-        var pack = new ShelfPack({ w: dim, h: dim });
-        var ok = true;
-        for (var j = 0; j < N; j++) {
-            ok = pack.allocate(fixedBoth[j].width, fixedBoth[j].height);
-            if (!ok) throw 'out of space';
-        }
+        var pack = new ShelfPack({ w: dim, h: dim }).pack(fixedBoth);
+        // var ok = true;
+        // for (var j = 0; j < N; j++) {
+        //     ok = pack.allocate({ w: fixedBoth[j].width, h: fixedBoth[j].height });
+        //     if (!ok) throw 'out of space';
+        // }
     })
     .add('ShelfPack allocate random width bins', function(t) {
-        var pack = new ShelfPack({ w: dim, h: dim });
-        var ok = true;
-        for (var j = 0; j < N; j++) {
-            ok = pack.allocate(randWidth[j].width, randWidth[j].height);
-            if (!ok) throw 'out of space';
-        }
+        var pack = new ShelfPack({ w: dim, h: dim }).pack(randWidth);
+        // var ok = true;
+        // for (var j = 0; j < N; j++) {
+        //     ok = pack.allocate({ w: randWidth[j].width, h: randWidth[j].height });
+        //     if (!ok) throw 'out of space';
+        // }
     })
     .add('ShelfPack allocate random height bins', function(t) {
-        var pack = new ShelfPack({ w: dim, h: dim });
-        var ok = true;
-        for (var j = 0; j < N; j++) {
-            ok = pack.allocate(randHeight[j].width, randHeight[j].height);
-            if (!ok) throw 'out of space';
-        }
+        var pack = new ShelfPack({ w: dim, h: dim }).pack(randHeight);
+        // var ok = true;
+        // for (var j = 0; j < N; j++) {
+        //     ok = pack.allocate({ w: randHeight[j].width, h: randHeight[j].height });
+        //     if (!ok) throw 'out of space';
+        // }
     })
     .add('ShelfPack allocate random height and width bins', function(t) {
-        var pack = new ShelfPack({ w: dim, h: dim });
-        var ok = true;
-        for (var j = 0; j < N; j++) {
-            ok = pack.allocate(randBoth[j].width, randBoth[j].height);
-            if (!ok) throw 'out of space';
-        }
+        var pack = new ShelfPack({ w: dim, h: dim }).pack(randBoth);
+        // var ok = true;
+        // for (var j = 0; j < N; j++) {
+        //     ok = pack.allocate({ w: randBoth[j].width, h: randBoth[j].height });
+        //     if (!ok) throw 'out of space';
+        // }
     })
     .add('BinPack allocate fixed bins', function(t) {
         var pack = BinPack(fixedBoth);
--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 
 module.exports = ShelfPack;
 
+
 /**
+ * Create a new ShelfPack bin allocator.
+ *
  * Uses the Shelf Best Height Fit algorithm from
  * http://clb.demon.fi/files/RectangleBinPack.pdf
  *
- * @class ShelfPack
- * @param {Object} options
- * @param {number} [options.width=64] Initial width of the sprite
- * @param {number} [options.height=64] Initial width of the sprite
- * @param {boolean} [options.autoGrow=false] If `true`, the sprite will automatically grow
+ * @class  ShelfPack
+ * @param  {Object}  options
+ * @param  {number}  [options.width=64]        Initial width of the sprite
+ * @param  {number}  [options.height=64]       Initial width of the sprite
+ * @param  {boolean} [options.autoGrow=false]  If `true`, the sprite will automatically grow
  * @example
- * var sprite = new ShelfPack({width: 64, height: 64});
+ * var sprite = new ShelfPack({ width: 64, height: 64 });
  */
 function ShelfPack(options) {
     options = options || {};
-    this.width = options.width || options.w || 64;
-    this.height = options.height || options.h || 64;
+    this.w = options.w || options.width || 64;
+    this.h = options.h || options.height || 64;
     this.autoResize = !!options.autoResize;
     this.shelves = [];
     this.stats = {};
@@ -27,12 +30,19 @@ function ShelfPack(options) {
 }
 
 /**
- * Batch bin allocator
+ * Batch allocate multiple bins into the sprite.
  *
- * @param {Array} bins Array of requested bins - each object should have `width` and `height` properties
- * @param {Object} options
- * @param {boolean} [options.inPlace=false] If `true`, the supplied bin objects will be updated inplace with `x` and `y` properties
- * @returns {Array} Array of allocated bins - each bin is an object with `x`, `y`, `w`, `h` properties
+ * @param   {Array}   bins Array of requested bins - each object should have `width` and `height` properties
+ * @param   {Object}  options
+ * @param   {boolean} [options.inPlace=false] If `true`, the supplied bin objects will be updated inplace with `x` and `y` properties
+ * @returns {Array}   Array of allocated bins - each bin is an object with `x`, `y`, `w`, `h` properties
+ * @example
+ * var bins = [
+ *     { id: 'a', width: 12, height: 12 },
+ *     { id: 'b', width: 12, height: 16 },
+ *     { id: 'c', width: 12, height: 24 }
+ * ];
+ * var results = sprite.pack(bins, { inPlace: true });
  */
 ShelfPack.prototype.pack = function(bins, options) {
     bins = [].concat(bins);
@@ -42,10 +52,10 @@ ShelfPack.prototype.pack = function(bins, options) {
         w, h, allocation;
 
     for (var i = 0; i < bins.length; i++) {
-        w = bins[i].width || bins[i].w;
-        h = bins[i].height || bins[i].h;
+        w = bins[i].w || bins[i].width;
+        h = bins[i].h || bins[i].height;
         if (w && h) {
-            allocation = this.allocate(w, h);
+            allocation = this.allocate({ w: w, h: h });
             if (!allocation) {
                 continue;
             }
@@ -60,36 +70,40 @@ ShelfPack.prototype.pack = function(bins, options) {
     return results;
 };
 
-
 /**
- * Single bin allocator
+ * Allocate a single bin into the sprite.
  *
- * @param {number} reqWidth Requested bin width
- * @param {number} reqHeight Requested bin height
- * @returns {Object} Allocated bin object with `x`, `y`, `w`, `h` properties, or `null` if allocation failed
+ * @param   {Object}  options
+ * @param   {number}  options.width   Width of the bin to allocate
+ * @param   {number}  options.height  Height of the bin to allocate
+ * @returns {Object}  Allocated bin object with `x`, `y`, `w`, `h` properties, or `null` if allocation failed
+ * @example
+ * var results = sprite.allocate({ width: 12, height: 16 });
  */
-ShelfPack.prototype.allocate = function(reqWidth, reqHeight) {
-    var y = 0,
+ShelfPack.prototype.allocate = function(options) {
+    var w = options.w || options.width,
+        h = options.h || options.height,
+        y = 0,
         best = { shelf: -1, waste: Infinity },
         shelf, waste;
 
-    // find shelf
+    // find the best shelf
     for (var i = 0; i < this.shelves.length; i++) {
         shelf = this.shelves[i];
-        y += shelf.height;
+        y += shelf.h;
 
         // exactly the right height with width to spare, pack it..
-        if (reqHeight === shelf.height && reqWidth <= shelf.free) {
-            this.count(reqHeight);
-            return shelf.alloc(reqWidth, reqHeight);
+        if (h === shelf.h && w <= shelf.free) {
+            this.count(h);
+            return shelf.alloc({ w: w, h: h });
         }
         // not enough height or width, skip it..
-        if (reqHeight > shelf.height || reqWidth > shelf.free) {
+        if (h > shelf.h || w > shelf.free) {
             continue;
         }
         // maybe enough height or width, minimize waste..
-        if (reqHeight < shelf.height && reqWidth <= shelf.free) {
-            waste = shelf.height - reqHeight;
+        if (h < shelf.h && w <= shelf.free) {
+            waste = shelf.h - h;
             if (waste < best.waste) {
                 best.waste = waste;
                 best.shelf = i;
@@ -99,69 +113,113 @@ ShelfPack.prototype.allocate = function(reqWidth, reqHeight) {
 
     if (best.shelf !== -1) {
         shelf = this.shelves[best.shelf];
-        this.count(reqHeight);
-        return shelf.alloc(reqWidth, reqHeight);
+        this.count(h);
+        return shelf.alloc({ w: w, h: h });
     }
 
     // add shelf
-    if (reqHeight <= (this.height - y) && reqWidth <= this.width) {
-        shelf = new Shelf(y, this.width, reqHeight);
+    if (h <= (this.h - y) && w <= this.w) {
+        shelf = new Shelf({ y: y, w: this.w, h: h });
         this.shelves.push(shelf);
-        this.count(reqHeight);
-        return shelf.alloc(reqWidth, reqHeight);
+        this.count(h);
+        return shelf.alloc({ w: w, h: h });
     }
 
     // no more space
     return null;
 };
 
-
 /**
- * Resizes the sprite
- * The resize will fail if the requested dimensions are smaller than the current sprite dimensions
+ * Resize the sprite.
+ * The resize will fail if the requested dimensions are smaller than the current sprite dimensions.
  *
- * @param {number} reqWidth Requested sprite width
- * @param {number} reqHeight Requested sprite height
+ * @param   {Object}  options
+ * @param   {number}  options.width   Requested new sprite width
+ * @param   {number}  options.height  Requested new sprite height
  * @returns {boolean} true if resize succeeded, false if failed
+ * @example
+ * sprite.resize({ width: 512, height: 512 });
  */
-ShelfPack.prototype.resize = function(reqWidth, reqHeight) {
-    if (reqWidth < this.width || reqHeight < this.height) {
+ShelfPack.prototype.resize = function(options) {
+    var w = options.w || options.width,
+        h = options.h || options.height;
+
+    if (w < this.w || h < this.h) {
         return false;
     }
-    this.height = reqHeight;
-    this.width = reqWidth;
+
+    this.w = w;
+    this.h = h;
     for (var i = 0; i < this.shelves.length; i++) {
-        this.shelves[i].resize(reqWidth);
+        this.shelves[i].resize(w);
     }
     return true;
 };
 
 
-function Shelf(y, width, height) {
-    this.y = y;
+
+/**
+ * Create a new Shelf.
+ *
+ * @private
+ * @class  Shelf
+ * @param  {Object}  options
+ * @param  {number}  options.y       Top coordinate of the new shelf
+ * @param  {number}  options.width   Width of the new shelf
+ * @param  {number}  options.height  Height of the new shelf
+ * @example
+ * var shelf = new Shelf({ y: 24, width: 512, height: 24 });
+ */
+function Shelf(options) {
+    var y = options.y,
+        w = options.width || options.w,
+        h = options.height || options.h;
+
     this.x = 0;
-    this.width = this.free = width;
-    this.height = height;
+    this.y = y;
+    this.w = this.free = w;
+    this.h = h;
 }
 
-Shelf.prototype = {
-    alloc: function(reqWidth, reqHeight) {
-        if (reqWidth > this.free || reqHeight > this.height) {
-            return null;
-        }
-        var x = this.x;
-        this.x += reqWidth;
-        this.free -= reqWidth;
-        return { x: x, y: this.y, w: reqWidth, h: reqHeight, width: reqWidth, height: reqHeight };
-    },
+/**
+ * Allocate a single bin into the shelf.
+ *
+ * @private
+ * @param   {Object}  options
+ * @param   {number}  options.width   Width of the bin to allocate
+ * @param   {number}  options.height  Height of the bin to allocate
+ * @returns {Object}  Allocated bin object with `x`, `y`, `w`, `h` properties, or `null` if allocation failed
+ * @example
+ * shelf.alloc({ width: 12, height: 16 });
+ */
+Shelf.prototype.alloc = function(options) {
+    var w = options.w || options.width,
+        h = options.h || options.height;
 
-    resize: function(reqWidth) {
-        if (reqWidth < this.width) {
-            return false;
-        }
-        this.free += (reqWidth - this.width);
-        this.width = reqWidth;
-        return true;
+    if (w > this.free || h > this.h) {
+        return null;
     }
+    var x = this.x;
+    this.x += w;
+    this.free -= w;
+    return { x: x, y: this.y, w: w, h: h, width: w, height: h };
 };
 
+/**
+ * Resize the shelf.
+ * The resize will fail if the requested width is smaller than the current shelf width.
+ *
+ * @private
+ * @param   {number}  w  Requested new width of the shelf
+ * @returns {boolean} true if resize succeeded, false if failed
+ * @example
+ * shelf.resize(512);
+ */
+Shelf.prototype.resize = function(w) {
+    if (w < this.w) {
+        return false;
+    }
+    this.free += (w - this.w);
+    this.w = w;
+    return true;
+};
